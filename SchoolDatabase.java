ef35a1c568b4ac5215bda594385f8680d7253279
stanng66 <stan.ng66@gmail.com>
2026-07-27 15:30:31 -0400

    Setup SchoolDatabase.java

---
+++ SchoolDatabase.java
@@ -0,0 +1,13 @@
+// ====================
+// Project
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Ronak Sheth
+// August 16, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+public class SchoolDatabase {
+
+}
