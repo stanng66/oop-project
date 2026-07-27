34ab9c9bc09541cedb076cd48bf56b2926eb2135
stanng66 <stan.ng66@gmail.com>
2026-07-27 15:17:08 -0400

    Add Main.java with basic structure
    Initial commit of the Main class with a main method.

---
+++ Main.java
@@ -0,0 +1,16 @@
+// ====================
+// Project
+// Stanley Nguyen
+// Humber College
+// CPAN-131-RNA
+// Ronak Sheth
+// August 16, 2026
+// --------------------
+// This program demonstrates 
+// ====================
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
